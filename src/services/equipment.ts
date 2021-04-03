55a3d57c2539c5a514d49444e874fa86e55aae13
Raf Ceuls <rceuls@users.noreply.github.com>
2021-04-03 15:11:27 +0200

    added equipment

--- src/services/equipment.ts
+++ src/services/equipment.ts
@@ -17,9 +17,9 @@ const basePack = [ "Backpack",
     `${getRndInteger(1, 6) * 10} gold pieces` ];
 
 export const fighterPacks: IEquipmentPack[] = [
-    { equipment: ["Chain Mail", "Shield", "Longsword", ...basePack],acMod : +5 },
-    { equipment: ["Chain Mail", "Polearm", ...basePack],acMod : +4 },
-    { equipment: ["Leather Armor", "Longsword", "Shortbow", "Quiver with 30 arrows", ...basePack], acMod : +2 },
+    { equipment: ["Chain Mail (AC 14)", "Shield (AC +1)", "Longsword (1d8 + STR)", ...basePack],acMod : +5 },
+    { equipment: ["Chain Mail (AC 14)", "Polearm (1d10 + STR, two handed, brace, slow)", ...basePack],acMod : +4 },
+    { equipment: ["Leather Armor (AC 12)", "Longsword (1d8 + STR)", "Shortbow (1d6, 5'-50' (+1 to hit)/51'-100'/101'-150' (-1 to hit))", "Quiver with 30 arrows", ...basePack], acMod : +2 },
 ];
 
 export const elfPacks: IEquipmentPack[] = [
@@ -29,17 +29,17 @@ export const elfPacks: IEquipmentPack[] = [
 ]
 
 export const magicUserPacks: IEquipmentPack[] = [
-    {equipment: [`Spellbook with read magic, ${spells[getRndInteger(0, spells.length - 1)]}`, `Scroll of ${spells[getRndInteger(0, spells.length - 1)]}`, "2 Daggers", "Walking Staff", ...basePack], acMod: +0},
-    {equipment: [`Spellbook with read magic, ${spells[getRndInteger(0, spells.length - 1)]}`, "50 GP", "2 Daggers", "Walking Staff", ...basePack], acMod: +0},
+    {equipment: [`Spellbook with read magic, ${spells[getRndInteger(0, spells.length - 1)]}`, `Scroll of ${spells[getRndInteger(0, spells.length - 1)]}`, "2 Daggers (1d4 + STR, 5'-10' (+1 to hit)/11'-20'/21'-30' (-1 to hit))", "Walking Staff  (1d4 + STR), slow, two handed) ", ...basePack], acMod: +0},
+    {equipment: [`Spellbook with read magic, ${spells[getRndInteger(0, spells.length - 1)]}`, "50 GP", "2 Daggers (1d4 + STR, 5'-10' (+1 to hit)/11'-20'/21'-30' (-1 to hit))", "Walking Staff (1d4 + STR, slow, two handed)", ...basePack], acMod: +0},
 ];
 
 
 export const clericPacks: IEquipmentPack[] = [
-    {equipment: ["Leather armor", "Shield", "Holy Symbol", "Vial of holy water",  "Mace", ...basePack], acMod: +2},
-    {equipment: ["Leather armor", "Sling", "30 sling bullets in pouch", "Holy Symbol", "Vial of holy water",  "Mace", ...basePack], acMod: +2},
+    {equipment: ["Leather armor (AC 12)", "Shield (AC +1)", "Holy Symbol", "Vial of holy water",  "Mace (1d6)", ...basePack], acMod: +3},
+    {equipment: ["Leather armor (AC 12)", "Sling (1d4 + STR, 5'-40' (+1 to hit)/41'-80'/81'-160' (-1 to hit))", "30 sling bullets in pouch", "Holy Symbol", "Vial of holy water",  "Mace (1d6)", ...basePack], acMod: +2},
 ]
 
 
 export const thiefPacks: IEquipmentPack[] = [
-    {equipment: ["Leather armor", "Thieves' tools", "Shortsword", "2 Daggers", "Silken Rope, 50'", ...basePack], acMod: +2},
+    {equipment: ["Leather armor (AC 12)", "Thieves' tools", "Shortsword (1d6)", "2 Daggers (1d4 + STR, 5'-10' (+1 to hit)/11'-20'/21'-30' (-1 to hit))", "Silken Rope, 50'", ...basePack], acMod: +2},
 ]
