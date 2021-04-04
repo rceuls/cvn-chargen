6328d4a78609609dbf4f925e4f2d6354128946ec
Raf Ceuls <rceuls@gmail.com>
2021-04-04 20:51:07 +0200

    refactor: minor refactoring

--- src/AttributesOverview.tsx
+++ src/AttributesOverview.tsx
@@ -1,41 +1,34 @@
+import { CharacterAttributes } from './services/character.classes';
 import { ICharacter } from './services/character.generator';
 import { formatBonus, getModifier } from './services/util';
+
+const attributeOrder: CharacterAttributes[] = [
+  CharacterAttributes.STR,
+  CharacterAttributes.DEX,
+  CharacterAttributes.CON,
+  CharacterAttributes.INT,
+  CharacterAttributes.WIS,
+  CharacterAttributes.CHA,
+];
 
 const AttributesOverview = ({ character }: { character: ICharacter }) => {
   return (
     <>
       <div className="row">
         <div className="col-sm">
-          <span className="font-weight-bold">STR</span>
-          <span className="ml-2">
-            {character.attributes.str} (
-            {formatBonus(getModifier(character.attributes.str))})
-          </span>
-          <span className="ml-2 font-weight-bold">DEX</span>
-          <span className="ml-2">
-            {character.attributes.dex} (
-            {formatBonus(getModifier(character.attributes.dex))})
-          </span>
-          <span className="ml-2 font-weight-bold">CON</span>
-          <span className="ml-2">
-            {character.attributes.con} (
-            {formatBonus(getModifier(character.attributes.con))})
-          </span>
-          <span className="ml-2 font-weight-bold">INT</span>
-          <span className="ml-2">
-            {character.attributes.int} (
-            {formatBonus(getModifier(character.attributes.int))})
-          </span>
-          <span className="ml-2 font-weight-bold">WIS</span>
-          <span className="ml-2">
-            {character.attributes.wis} (
-            {formatBonus(getModifier(character.attributes.wis))})
-          </span>
-          <span className="ml-2 font-weight-bold">CHA</span>
-          <span className="ml-2">
-            {character.attributes.cha} (
-            {formatBonus(getModifier(character.attributes.cha))})
-          </span>
+          {attributeOrder.map((a: CharacterAttributes) => {
+            return (
+              <>
+                <span className="font-weight-bold">
+                  {CharacterAttributes[a]}
+                </span>
+                <span className="ml-2 mr-2">
+                  {character.attributesArray[+a]} (
+                  {formatBonus(getModifier(character.attributesArray[+a]))})
+                </span>
+              </>
+            );
+          })}
         </div>
       </div>
       <div className="row">
